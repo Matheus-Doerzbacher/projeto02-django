4c634d8bf9865caf21612c7bc86dcc8e78d15646
Matheus Doerzbacher <matheus.doer@gmail.com>
2024-04-11 16:38:50 -0300

    Criado um formulario de Contato

--- baseApp/views.py
+++ baseApp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .forms import ContatoForm
 
 
 def index(request):
@@ -6,7 +8,29 @@ def index(request):
 
 
 def contato(request):
-    return render(request, "contato.html")
+    form = ContatoForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            nome = form.cleaned_data["nome"]
+            email = form.cleaned_data["email"]
+            assunto = form.cleaned_data["assunto"]
+            mensagem = form.cleaned_data["mensagem"]
+
+            print("Mensaem Enviada")
+            print(f"Nome : {nome}")
+            print(f"Email : {email}")
+            print(f"Assunto : {assunto}")
+            print(f"Mensagem : {mensagem}")
+
+            messages.success(request, "E-mai enviado com sucesso")
+            form = ContatoForm()
+    else:
+        messages.error(request, "Erro ao enviar Email")
+    context = {
+        "form": form,
+    }
+    return render(request, "contato.html", context)
 
 
 def produto(request):
